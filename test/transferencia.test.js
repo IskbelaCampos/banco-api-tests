329042fb165d5a70f78fe7063c5e51cb158cccfe
Isabela <iskbela.cristina@gmail.com>
2026-03-23 23:52:03 +0000

    Adicionando fixtures para gerir os corpos das requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const {expect} =  require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferências', () => {
     describe('POST /transferencias', () => {
@@ -11,31 +12,26 @@ describe('Transferências', () => {
         })
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for acima de R$10.00', async () => {
-            
+            const bodyTransferencia = { ...postTransferencias}
+
             const response = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', 'Bearer ' + token)
-                .send({
-                    "contaOrigem": 1,
-                    "contaDestino": 2,
-                    "valor": 11,
-                    "token": ""
-                })
+                .send(bodyTransferencia)
+
                 expect(response.status).to.equal(201);
         })
         it('Deve retonar falha com 422 quando o valor for abaixo de R$10.00 ', async () => {
+            const bodyTransferencia = { ...postTransferencias}
+            bodyTransferencia.valor = 5.99
 
             const response = await request(process.env.BASE_URL)
               .post('/transferencias')
               .set('Content-Type', 'application/json')
               .set('Authorization', 'Bearer ' + token)
-              .send({
-                  "contaOrigem": 1,
-                  "contaDestino": 2,
-                  "valor": 5.99,
-                  "token": ""
-              })
+              .send(bodyTransferencia)
+
               expect(response.status).to.equal(422);
         })
     })
